e2ac9f9b2815c6865bb99114b04cab6d80dc77aa
newgenstudios12 <newgenstudios12@gmail.com>
2026-08-22 01:16:12 +0800

    fix(cache): invalidate stale VCCF service worker cache

--- public/vccf-sw.js
+++ public/vccf-sw.js
@@ -1,4 +1,4 @@
-const CACHE='vccf-connect-v1';
+const CACHE='vccf-connect-v2';
 const APP=['/','/manifest.webmanifest','/vccf-suite.css','/vccf-suite.js','/vccf-ui-refresh.css','/vccf-theme.js','/vccf-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
