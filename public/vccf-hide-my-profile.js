6e5fb3645cb5453742556f716623ce6b57dfed18
newgenstudios12 <newgenstudios12@gmail.com>
2026-08-21 21:55:44 +0800

    chore(nav): remove redundant profile cleanup script

--- public/vccf-hide-my-profile.js
+++
@@ -1,5 +0,0 @@
-/* VCCF: legacy My Profile de-duplication disabled. The navigation cleanup now
-   removes the legacy entry and restores the dedicated My Profile v2 control. */
-(function () {
-  'use strict';
-})();
