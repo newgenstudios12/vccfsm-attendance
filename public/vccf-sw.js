1cbe9e3b04be4ca6a5d30a4826e0f8ba64f347da
newgenstudios12 <newgenstudios12@gmail.com>
2026-09-04 09:31:12 +0800

    feat(pwa): activate resized VCCF app icons

--- public/vccf-sw.js
+++ public/vccf-sw.js
@@ -1,5 +1,5 @@
 const APP_URL='/';
-const APP_ICON='/vccf-app-icon.svg';
+const APP_ICON='/vccf-app-icon-192x192.png?v=20260904-3';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
