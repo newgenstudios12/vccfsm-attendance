c75bea9e70d2df9ad055fbcbb8bfbad4e9e6f7a5
newgenstudios12 <newgenstudios12@gmail.com>
2026-09-07 08:40:44 +0800

    Bust Add Member cache for member creation hotfix

--- public/vccf-members-add-loader.js
+++ public/vccf-members-add-loader.js
@@ -31,7 +31,7 @@ function loadModule() {
   ensureStyles();
   loading = new Promise(resolve => {
     const script = document.createElement('script');
-    script.src = '/vccf-members-add.js?v=20260905-4';
+    script.src = '/vccf-members-add.js?v=20260907-1';
     script.dataset.vccfMembersAddModule = '1';
     script.onload = () => { loaded = true; loading = null; activate(); resolve(true); };
     script.onerror = () => { loading = null; console.error('Add Member module failed to load. The rest of VCCF Connect remains available.'); resolve(false); };
