39be9be15ed1c56e9fe4783da4dd67db0aa27d3a
newgenstudios12 <newgenstudios12@gmail.com>
2026-09-18 15:06:12 +0800

    Refresh enhanced attendance asset version

--- public/service-attendance.js
+++ public/service-attendance.js
@@ -106,7 +106,7 @@ window.VCCFServiceAttendance={mount,unmount,refresh:renderRecords};
 
 (()=>{
   const load=(src,key,next)=>{if(document.querySelector(`script[data-${key}]`)){next?.();return}const s=document.createElement('script');s.src=src;s.async=false;s.setAttribute(`data-${key}`,'1');s.onload=()=>next?.();document.head.appendChild(s)};
-  load('/vccf-service-attendance-v2.js?v=20260914-1','vccf-service-attendance-v2',()=>
+  load('/vccf-service-attendance-v2.js?v=20260918-2','vccf-service-attendance-v2',()=>
     load('/vccf-bible-study-barangay-dropdown.js?v=20260914-1','vccf-bible-study-dropdown',()=>
       load('/vccf-bible-study-barangay-base.js?v=20260914-1','vccf-bible-study-base',()=>
         load('/vccf-extra-attendance-checklists.js?v=20260914-1','vccf-extra-attendance-checklists'))));
